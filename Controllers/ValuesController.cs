1c2c59a74a533def139a65b063bc09ac66bbddbc
weszty <weszty@yahoo.com>
2020-02-13 12:02:05 +0200

    Authentification middleware

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     //POST http://localhost:5000/api/values
     [Route("api/[controller]")]
     [ApiController]
@@ -28,7 +30,7 @@ namespace DatingApp.API.Controllers
             return Ok(values);
         }
 
-        // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
